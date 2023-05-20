41c8245a33840d502b37e2fbde74b74175595881
BhavyaBh289 <shahbhavyasp@gmail.com>
2023-05-20 22:32:08 +0530

    output

--- _1.java
+++ _1.java
@@ -1,3 +1,14 @@
-public class _1 {
+import java.util.Scanner;
 
+public class _1 {
+    public static void main (String [] args ){
+        Scanner sc = new Scanner(System.in);
+        int []  array = new int[sc.nextInt()];
+        for (int i=0; i<array.length;i++){
+            array[i]= sc.nextInt();
+        }
+        for (int i=0; i<array.length;i++){
+            System.out.print(array[i]+" ");
+        }
+    }
 }
